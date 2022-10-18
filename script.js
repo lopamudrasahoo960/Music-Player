c10aec856531662aea1f64879dfd36b3061aced4
lopamudrasahoo960 <98395044+lopamudrasahoo960@users.noreply.github.com>
2022-10-18 16:40:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -22,28 +22,28 @@ let updateTimer;
 
 const music_list = [
     {
-        img : '/images/stay.png',
+        img : './images/stay.png',
         name : 'Stay',
         artist : 'The Kid LAROI, Justin Bieber',
-        music : '/music/stay.mp3'
+        music : './music/stay.mp3'
     },
     {
-        img : '/images/fallingdown.jpg',
+        img : './images/fallingdown.jpg',
         name : 'Falling Down',
         artist : 'Wid Cards',
-        music : '/music/fallingdown.mp3'
+        music : './music/fallingdown.mp3'
     },
     {
-        img : '/images/faded.png',
+        img : './images/faded.png',
         name : 'Faded',
         artist : 'Alan Walker',
-        music : '/music/Faded.mp3'
+        music : './music/Faded.mp3'
     },
     {
-        img : '/images/ratherbe.jpg',
+        img : './images/ratherbe.jpg',
         name : 'Rather Be',
         artist : 'Clean Bandit',
-        music : '/music/Rather Be.mp3'
+        music : './music/Rather Be.mp3'
     }
 ];
 
